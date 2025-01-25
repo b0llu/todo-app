e4c0998cefba683d1d6358d9cad5a070d0561624
Dhruv <samantdhruv@gmail.com>
2025-01-25 08:48:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          <button onClick={() => setCount(count + 1)}>Click me</button>
+        </p>
+        <p>Count: {count}</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
