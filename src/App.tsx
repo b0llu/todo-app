ee13e7c4b602354cbab2d24ffa8822e545274420
Dhruv <samantdhruv@gmail.com>
2025-01-25 09:26:39 +0530

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="text-2xl text-red-400">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>
